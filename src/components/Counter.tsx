560eb7277fc5429b442c51d03e3e581843405041
Christian99909 <cristian99909@gmail.com>
2025-12-23 13:28:33 -0600

    feat: Add Solid.js counter component and integrate it into an Astro page using client-side hydration.

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,16 +1,19 @@
-import { createSignal } from "solid-js"
+import { createSignal, type Component, type JSX } from "solid-js"
 
+interface Props {
+    initialValue: number;
+    children?: JSX.Element;
+}
 
+export const Counter: Component<Props> = (props) => {
 
-export const Counter = () => {
-
-    const [counter, setCounter] = createSignal(10)
+    const [counter, setCounter] = createSignal(props.initialValue);
 
 
     return (
         <>
 
-            <h1 class="text-5xl">Counter</h1>
+            {props.children}
             <h3 class="text-xl">Value: {counter()}</h3>
 
             <button onClick={() => setCounter(prev => ++prev)} class="bg-blue-500 p-2 mr-2 rounded">+1</button>
